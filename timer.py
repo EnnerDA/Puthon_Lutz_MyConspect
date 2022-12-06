9e3c1f4d3095913eca368fed14fcf5b6bd70f4a4
Mitya <116806816+EnnerDA@users.noreply.github.com>
2022-12-06 17:36:40 +0500

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,37 @@
+import time, sys
+timer = time.time
+def total(reps, func, *pargs, **kargs):
+    '''Сумарное время выполнения функции func() reps раз.
+        Возвращает суммарное время и последний результат'''
+    repslist = list(range(reps))
+    start = timer()
+    for i in repslist:
+        ret = func(*pargs, **kargs)
+    elapsed = timer() - start
+    return elapsed, ret
+
+def bestof(reps, func, *pargs, **kargs):
+    '''Самая быстрая функция func() среди reps запусков. 	
+    Возвращает (лучшее время, последний результат)'''
+    besttime = float('inf')
+    bedtime = 0
+    for i in range(reps):
+        start = timer()
+        ret = func(*pargs, **kargs)
+        elapsed = timer() - start
+        if elapsed < besttime:
+            besttime = elapsed
+        if elapsed > bedtime:
+            bedtime = elapsed
+        
+    print(f'\tFrom {func.__name__} in {reps} reps:')
+    if len(str(ret)) < 60:
+        print(f'besttime = {besttime}, \nbedtime = {bedtime}, \nresult = {ret}')
+    else:
+        print(f'''besttime = {besttime} \nbedtime = {bedtime} \nto many symbols to write the result...''')
+    
+
+def bestoftotal(reps1, reps2, func, *pargs, **kargs):
+    '''Лучшее суммарное время:
+    (лучшее время из repsl запусков (суммарное время reps2 запусков func))'''	
+    return bestof(reps1, total, reps2, func, *pargs, **kargs)
