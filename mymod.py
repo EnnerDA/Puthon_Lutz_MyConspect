964d06ec9445c86fa91c5c3c0647fbb581e8c7f5
Mitya <116806816+EnnerDA@users.noreply.github.com>
2022-12-12 17:58:29 +0500

    Add files via upload

---
+++ mymod.py
@@ -0,0 +1,32 @@
+def countLines(file_name):
+    res = len(open(r''+file_name).readlines())
+    print('There are', res, 'lines in', file_name)
+
+def countChars(file_name):
+    res = 0
+    for line in open(r''+file_name).readlines():
+        res += len(line)
+    print('There are', res, 'chars in', file_name)
+    
+def tester():
+    # вставить возможность подхватывать код из командной строки, мать её
+    try:
+        fname = input('Введиет имя файла, или жмякните Enter\n')
+        if fname.strip() == '': fname = 'mymod.py'
+        countLines(fname)
+        countChars(fname)
+    except:
+        print('Мы не нашли что вы хотели, поэтому протестим на этом же файле')
+        fname = 'mymod.py'
+        countLines(fname)
+        countChars(fname)
+        
+
+if __name__ == '__main__':
+    tester()
+    
+            
+            
+
+
+
